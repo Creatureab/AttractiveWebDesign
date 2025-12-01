b8ed4e72a1bdd0a21388755478533415b044aa47
Animesh <animeshbhople885507@gmail.com>
2025-12-01 10:56:24 +0530

    this testing commit

---
+++ src/database/index.ts
@@ -0,0 +1,8 @@
+﻿export { default as Event } from './event.model';
+export type { IEvent } from './event.model';
+export { default as Booking } from './booking.model';
+export type { IBooking } from './booking.model';
+
+
+
+
