13c0ad8e48b76612c1322d04ce9ae5031b9da58b
Animesh <animeshbhople885507@gmail.com>
2025-12-05 01:10:38 +0530

    base url changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import EventCards from "@/components/EventCards";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database/event.model";
 import { cacheLife } from "next/cache";
+import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    const response = await fetch(`${BASE_URL}/api/events`);
-    const { events } = await response.json();
+    //const response = await fetch(`${BASE_URL}/api/events`);
+    //const { events } = await response.json();
 
     return (
         <section>
